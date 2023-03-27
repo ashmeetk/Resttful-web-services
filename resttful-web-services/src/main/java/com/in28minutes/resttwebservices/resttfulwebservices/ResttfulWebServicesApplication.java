5c772ceeb1ed069b88adedf6ec073ae758a18fff
ashmeetk <34856693+ashmeetk@users.noreply.github.com>
2023-03-27 22:38:09 +0530

    Add files via upload

---
+++ resttful-web-services/src/main/java/com/in28minutes/resttwebservices/resttfulwebservices/ResttfulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.resttwebservices.resttfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResttfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResttfulWebServicesApplication.class, args);
+	}
+
+}
---
+++ resttful-web-services/src/test/java/com/in28minutes/resttwebservices/resttfulwebservices/ResttfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.resttwebservices.resttfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResttfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
